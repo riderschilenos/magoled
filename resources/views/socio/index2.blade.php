1e3a8765b68bc5813c37bd086a979ec84a0ab664
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-07-15 01:35:39 -0400

    u

--- resources/views/socio/index2.blade.php
+++ resources/views/socio/index2.blade.php
@@ -11,7 +11,7 @@
         <div class="words sm:hidden bg-white pt-2 overflow-x-auto whitespace-no-wrap border-b-2 font-bold text-gray-700">
             <div class="ml-4 pl-2 flex justify-center">
                 <a href="">
-                    <div class="px-4 ml-12 py-2 cursor-pointer underline text-gray-900">Riders</div>
+                    <div class="px-4 ml-12 py-2 cursor-pointer underline text-gray-900">Magos</div>
                 </a>
                
                 <a href="{{Route('foros.index')}}">
@@ -20,9 +20,7 @@
                 <a href="{{Route('ticket.evento.index.desktop')}}">
                     <div class="px-4 py-2 cursor-pointer hover:underline"  >Eventos</div>
                 </a>
-                <a href="{{Route('garage.vehiculos.registerindex.desktop')}}">
-                    <div class="px-4 py-2 cursor-pointer hover:underline" >Bikes</div>
-                </a>
+            
                 <div class="px-4 py-2 cursor-pointer hover:underline text-gray-900" @click="user = false; home = false; video = true; socio = false; evento = false; registro = false; vendedor = false; base = false" >Videos</div>
                 <div class="px-4 py-2 cursor-pointer hover:underline" @click="user = false; novedades = true; home = false; socio = false; evento = false; registro = false; vendedor = false; base = false" >Novedades</div>
             
